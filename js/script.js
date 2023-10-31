076413f1fbb85e4fecdbda0fcf13e84118b5a01a
cqueiroga <chris.queiroga@gmail.com>
2023-10-31 08:09:21 -0400

    Created function to randomly select one of the options from the array [rock, paper, scissors]

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("asdf");
+function getComputerChoice() {
+	let options = ["Rock", "Paper", "Scissors"];
+	let random = Math.floor(Math.random() * 3);
+
+	return options[random];
+}
+
+const compChoice = getComputerChoice();
