a2f8e0661f77b0d23b1e0e0242f1260de8f18b34
cqueiroga <chris.queiroga@gmail.com>
2023-10-31 08:32:50 -0400

    Added playRound function Compared playerSelection to computerSelection and return message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
 function getComputerChoice() {
-	let options = ["Rock", "Paper", "Scissors"];
+	let options = ["rock", "paper", "scissors"];
 	let random = Math.floor(Math.random() * 3);
 
 	return options[random];
 }
 
-const compChoice = getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+		return "You Lose! Paper beats Rock";
+	} else if (
+		playerSelection.toLowerCase() == "rock" &&
+		computerSelection == "scissors"
+	) {
+		return "You Win! Rock beats Scissors";
+	} else if (
+		playerSelection.toLowerCase() == "scissors" &&
+		computerSelection == "rock"
+	) {
+		return "You Lose! Rock beats Scissors";
+	} else if (
+		playerSelection.toLowerCase() == "scissors" &&
+		computerSelection == "paper"
+	) {
+		return "You Win! Scissors beats Paper";
+	} else if (
+		playerSelection.toLowerCase() == "paper" &&
+		computerSelection == "rock"
+	) {
+		return "You Win! Paper beats Rock";
+	} else if (
+		playerSelection.toLowerCase() == "paper" &&
+		computerSelection == "scissors"
+	) {
+		return "You Lose! Scissors beats Paper";
+	} else {
+		return "It's a tie!";
+	}
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
